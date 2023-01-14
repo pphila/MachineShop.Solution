0d6d4e91b782f77e39bb0e66f53451a089717238
pphila <efl2ice89@gmail.com>
2023-01-14 10:55:17 -0800

    add initial index pages with views controllers

---
+++ Factory/Controllers/EngineersContoller.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Models
+{
+  public class EngineerController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineerController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+    
+  }
+}
