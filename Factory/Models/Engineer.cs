bd6a5707d9d89ed8deaad43d62a4a9ec11217641
pphila <efl2ice89@gmail.com>
2023-01-14 20:42:01 -0800

    add required field input with error message and fixed engineer constructor

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -6,10 +6,10 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "Please Enter a Name.")]
     public string Name { get; set; }
     public string Specialty { get; set; }
     public int MachineId { get; set; }
-    public Machine Machine { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,6 +6,7 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "Please Enter a Name.")]
     public string Name { get; set; }
     public string Type { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
